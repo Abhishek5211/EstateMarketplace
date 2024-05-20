115d98067a9c2f7a9e6180dba475f862654e46ed
Abhishek5211 <leweijr@outlook.com>
2024-05-20 22:36:44 +0545

    Adding Basic Routing Setpu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
- import React from 'react'
+ import React from 'react';
+ import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/about';
  
  export default function App() {
    return (
+    <BrowserRouter>
+    <Routes>
+
+      <Route path='./pages' element = {<Home />}/>
+      <Route path='./pages' element = {<SignIn />}/>
+      <Route path='./pages' element = {<SignUp />}/>
+      <Route path='./pages' element = {<About />}/>
+
      <div><h1 className = 'text-red'>App</h1></div>
+
+     </Routes>
+    </BrowserRouter>
    )
  }
  

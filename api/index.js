5e22d2995fc43f82c0134228f3a3945fa82a07b5
Abhishek5211 <leweijr@outlook.com>
2024-05-23 17:21:22 +0545

    adding user schema

--- api/index.js
+++ api/index.js
@@ -1,14 +1,16 @@
-import express from 'express';
-import mongoose from 'mongoose';
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
- 
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connected');
-});
-
-app.listen(3000, ()=>{
-
-});
+app.listen(3000, () => {});

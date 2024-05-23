490bc24dbd7b81bc95d5dd2bf6e1634bb65864be
Abhishek5211 <leweijr@outlook.com>
2024-05-23 16:52:40 +0545

    Connection with DB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
 
 const app = express();
+ 
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected');
+});
 
 app.listen(3000, ()=>{
 
-    
 });
--- clientApp/src/components/Header/Header.tsx
+++ clientApp/src/components/Header/Header.tsx
@@ -4,24 +4,26 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="shadow sticky z-50 top-0">
-      <nav className="bg-slate-300 border-gray-200 px-4 lg:px-6 py-2.5">
-        <div className="flex flex-wrap items-center justify-around mx-auto max-w-screen-xl">
-          <Link to="/" className="flex items-center">
-            <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-              <span className="text-slate-500">Rajesh</span>
-              <span className="text-slate-700">Estate</span>
-            </h1>
-          </Link>
-          <form className="bg-slate-100 rounded-lg flex items-center p-3">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="bg-transparent focus:outline-none w-24 sm:w-64"
-            />
-            <FaSearch className="text-slate-400 hover:cursor-pointer"></FaSearch>
-          </form>
+      <nav className="bg-slate-300 border-gray-200 px-4 lg:px-6 py-2.5 flex justify-even items-center">
           <div>
-            <ul className="flex flex-wrap gap-4">
+            <Link to="/" className="flex items-center px-4 lg:px-6">
+              <h1 className="font-bold text-sm sm:text-2xl flex flex-wrap">
+                <span className="text-slate-500">Rajesh</span>
+                <span className="text-slate-700">Estate</span>
+              </h1>
+            </Link>
+          </div>
+        <div className="flex flex-wrap items-center justify-between mx-auto max-w-screen-xl">
+          <div className="flex flex-wrap gap-8">
+            <form className="bg-slate-100 rounded-lg flex items-center p-3">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="bg-transparent focus:outline-none w-24 sm:w-64"
+              />
+              <FaSearch className="text-slate-400 hover:cursor-pointer"></FaSearch>
+            </form>
+            <ul className="flex flex-wrap gap-4 items-center">
               <Link to={"/home"}>
                 <li className="hidden sm:inline text-slate-700 hover:font-bold">
                   Home
